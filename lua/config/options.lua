ae26a79b5c6c2399ac7161e4c6d441b6d1d921a1
Trevor Hauter <trevor@hauter.dev>
2026-08-23 20:23:35 -0400

    Add comments to inital options file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,24 +1,32 @@
--- ~/.config/nvim/lua/config/options.lua
-vim.g.mapleader = " " -- Prefix used for many keybinds
-
-require("plugins") -- Will load plugins directory automatically
-
-vim.diagnostic.config({ virtual_text = true }) -- Enables lsp warnings
-
--- Personal preferences!
+-- Set default indentation length (in spaces)
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
 
-vim.o.winborder = "rounded"
-
+-- Use spaces for tab in insert mode ;)
 vim.opt.expandtab = true
-vim.opt.hlsearch = false
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.smartindent = true
-vim.opt.swapfile = false
-vim.opt.wrap = true
 
+-- Don't highlight every search result when performing a `/` search
+vim.opt.hlsearch = false
+
+-- Show line numbers in editor
+vim.opt.nu = true
+
+-- Make line numbers relative to cursor (really great for efficient motions!)
+vim.opt.relativenumber = true
+
+-- Always keep 8 lines of buffer at the top or bottom
+vim.opt.scrolloff = 8
+
+-- Leaves space for signs and other hints in line number column
+vim.opt.signcolumn = "yes"
+
+-- Better indentation
+vim.opt.smartindent = true
+
+-- I have swapfiles disabled for convenience, 
+-- may want to enable if memory is a concern
+vim.opt.swapfile = false
+
+-- Wrap text at edge of screen
+vim.opt.wrap = true
